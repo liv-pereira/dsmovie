caaab96de7f5fb3ed08424ec3709bf14049debc0
Lívia Pereira Pondé <liuponde@gmail.com>
2022-05-11 00:59:31 -0300

    MovieCard

--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -1,9 +1,39 @@
+import MovieCard from "Components/MovieCard";
+import MovieScore from "Components/MovieScore";
+import MovieStars from "Components/MovieStars";
 import Pagination from "Components/Pagination";
 
 function Listing (){
 
   return(
+    <>
     <Pagination/>
+
+    <div className="container">
+    <div className="row">
+      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+      <MovieCard/>
+      </div>
+      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+      <MovieCard/>
+      </div>
+      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+      <MovieCard/>
+      </div>
+      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+      <MovieCard/>
+      </div>
+      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+      <MovieCard/>
+      </div>
+      
+    </div>
+    </div>
+    
+
+
+    
+    </>
   );
 }
 export default Listing;
