81fbad7b2fe52bbbdc3e3ff40c3c65f7f6b95018
Lívia Pereira Pondé <liuponde@gmail.com>
2022-05-15 11:32:41 -0300

    useState, useEffect

--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -4,16 +4,28 @@ import MovieScore from "Components/MovieScore";
 import MovieStars from "Components/MovieStars";
 import Pagination from "Components/Pagination";
 import {BASE_URL} from "Utils/requests";
+import {useEffect, useState} from "react";
+import { MoviePage } from "Type/movie";
 
 function Listing (){
 
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() =>{
+    axios.get (`${BASE_URL}/movies?size=20&page=1`).then(response =>{
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+
+  }, []);
+
   //forma errada
-  axios.get (`${BASE_URL}/movies`).then(response =>{
-    console.log(response.data)
-  })
+ 
 
   return(
     <>
+    <p>{pageNumber}</p>
     <Pagination/>
 
     <div className="container">
